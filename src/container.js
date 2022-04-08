c492c2dbefde8e22b518e89603cf57ef99a0bf9b
rajeshmn47 <rajeshmn47@gmail.com>
2022-04-08 18:17:18 +0530

    last commit

---
+++ src/container.js
@@ -0,0 +1,14 @@
+import Maindiv from "./maindiv"
+
+export const Container=()=>{
+    return(
+        <>
+        <div className='container'>
+<Maindiv/>
+        </div>
+        
+        </>
+    )
+}
+
+export default Container
---
+++ src/maindiv.js
@@ -0,0 +1,15 @@
+export const Maindiv=()=>{
+    return(
+        <>
+<div className='maindiv'>
+    <div className="sidebar"></div>
+    <div className="rightbar">
+        
+    </div>
+</div>
+
+        </>
+    )
+}
+
+export default Maindiv

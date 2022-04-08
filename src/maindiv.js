8fd2f87bf8b1f24b64e6724271d5a0543dcdf955
rajeshmn47 <rajeshmn47@gmail.com>
2022-04-08 19:36:43 +0530

    last commit

--- src/maindiv.js
+++ src/maindiv.js
@@ -15,31 +15,31 @@ export const Maindiv=()=>{
 <h2>Zendentha</h2>
 <p>cabut digu transpa</p>
 </div>
-<SettingsOutlinedIcon/>
+<SettingsOutlinedIcon style={{color:'grey',opacity:'0.9'}}/>
         </div>
         <div>
 <div className='item'> 
-    <HelpOutlineOutlinedIcon/>
+    <HelpOutlineOutlinedIcon style={{color:'grey',opacity:'0.9'}}/>
 <h2>Overview </h2>
     </div>
     <div className='item'>
-        <EventNoteOutlinedIcon/>
+        <EventNoteOutlinedIcon style={{color:'grey',opacity:'0.9'}}/>
 <h2>Calender</h2>
     </div>
     <div className='item select'>
-        <PersonOutlineOutlinedIcon/>
+        <PersonOutlineOutlinedIcon />
 <h2>Patient List</h2>
     </div>
     <div className='item'>
-        <ChatRoundedIcon/>
+        <ChatRoundedIcon  style={{color:'grey',opacity:'0.9'}}/>
 <h2>Messages</h2>
     </div>
     <div className='item'>
-        <LocalAtmRoundedIcon/>
+        <LocalAtmRoundedIcon  style={{color:'grey',opacity:'0.9'}}/>
 <h2>Payment Information</h2>
     </div>
     <div className='item'>
-        <SettingsOutlinedIcon/>
+        <SettingsOutlinedIcon  style={{color:'grey',opacity:'0.9'}}/>
 <h2>Settings</h2>
     </div>
     </div>

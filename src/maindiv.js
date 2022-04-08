4287674634fc8571db946beb401a65178a544c9b
rajeshmn47 <rajeshmn47@gmail.com>
2022-04-08 18:56:55 +0530

    last commit

--- src/maindiv.js
+++ src/maindiv.js
@@ -1,10 +1,39 @@
+import HelpOutlineOutlinedIcon from '@material-ui/icons/HelpOutlineOutlined';
+import EventNoteOutlinedIcon from '@material-ui/icons/EventNoteOutlined';
+
 export const Maindiv=()=>{
     return(
         <>
 <div className='maindiv'>
-    <div className="sidebar"></div>
+    <div className="sidebar">
+        <div>
+<div> 
+    <HelpOutlineOutlinedIcon/>
+<h2>Overview </h2>
+    </div>
+    <div>
+        <EventNoteOutlinedIcon/>
+<h2>Calender</h2>
+    </div>
+    <div className='select'>
+<h2>Patient List</h2>
+    </div>
+    <div>
+<h2>Messages</h2>
+    </div>
+    <div>
+<h2>Payment Information</h2>
+    </div>
+    <div>
+<h2>Overview</h2>
+    </div>
+    </div>
+    <div>
+
+    </div>
+    </div>
     <div className="rightbar">
-        
+
     </div>
 </div>
 
